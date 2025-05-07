c47baa74aa3203a556aa33a83cc2281d7f913b2c
MustafaHunter <142630575+MustafaHunter@users.noreply.github.com>
2025-05-07 03:15:48 -0400

    Add files via upload

---
+++ AdamLibrary/Adam/client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import "./css/styles.css";
+const Home = (props) => {
+  return (
+    <>
+      <div class="body" className="body">
+         <h1 className="home">Welcome to The Library </h1>
+         <img
+  src="../images/library.webp"
+  alt="Library"
+  style={{ width: "100%", maxWidth: "800px", height: "auto" }}
+/>
+
+        <br/>
+    
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
